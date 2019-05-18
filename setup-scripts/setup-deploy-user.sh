848997d684d7ddc34e2dbdb06bff5f66dcd90663
DarkWasp <coderwasp@gmail.com>
2019-05-18 18:26:51 +0000

    Fixed typo on deploy user setup script

--- setup-scripts/setup-deploy-user.sh
+++ setup-scripts/setup-deploy-user.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo adduser deploy
 sudo usermod -aG sudo deploy
-echo "Copy and paste this snippet 'deploy ALL=(ALL) NOPASSWD:ALL' in the file about to be opend"
+echo "Copy and paste this snippet 'deploy ALL=(ALL) NOPASSWD:ALL' in the file about to be opened"
 read -n 1 -s -r -p "Press any key to open the file"
 sudo visudo -f /etc/sudoers.d/90-cloud-init-users
 echo "Deploy user setup successfully"
