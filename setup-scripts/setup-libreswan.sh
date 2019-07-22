dff3c92201b35597a9781d782d2e70f9c4b5b891
DarkWasp <coderwasp@gmail.com>
2019-07-22 12:16:53 +0000

    Updated libreswan setup script

--- setup-scripts/setup-libreswan.sh
+++ setup-scripts/setup-libreswan.sh
@@ -13,6 +13,7 @@ echo "net.ipv4.conf.default.send_redirects=0"
 echo "net.ipv4.conf.all.send_redirects = 0"
 
 sudo apt-get install -y git
+sudo rm -rf ~/libreswan
 pushd ~
 git clone https://github.com/libreswan/libreswan.git
 sudo apt-get install -y libnss3-dev libnspr4-dev pkg-config libpam-dev \
