e5ad527e3c6bfaeee07b804030e8305041534900
DarkWasp <coderwasp@gmail.com>
2019-10-31 13:15:23 +0000

    Updated rails env setup script with custom installations of some gems

--- setup-scripts/setup-rails-env.sh
+++ setup-scripts/setup-rails-env.sh
@@ -14,3 +14,15 @@ bundle config build.pg --with-pg-config=/usr/bin/pg_config
 echo "def clear;system('clear');end" >> ~/.irbrc
 echo "require 'hirb'" >> ~/.irbrc
 echo "Hirb.enable" >> ~/.irbrc
+
+# # CUSTOM INSTALL FOR POSTGRES GEM
+# sudo apt-get install postgresql-client libpq5 libpq-dev 
+# bundle config build.pg --with-pg-config=/usr/bin/pg_config
+
+# # CUSTOM INSTALL FOR NOKOGIRI GEM
+# sudo apt-get install libxml2-dev libxslt-dev
+# gem install nokogiri -- --use-system-libraries
+# bundle config build.nokogiri --use-system-libraries
+
+# # DEPENDENCIES FOR RMAGICK
+# sudo apt-get install build-essential imagemagick libmagickcore-dev libmagickwand-dev
