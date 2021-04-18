21de7ba792cdea48b1837637b1e8e87152f3b86a
George Osae <gkosae@gmail.com>
2021-04-18 19:59:08 +0000

    Accept username as an argument when setting up deploy user

--- setup-scripts/setup-deploy-user.sh
+++ setup-scripts/setup-deploy-user.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 set -e
 
-sudo adduser deploy
-sudo usermod -aG sudo deploy
-echo "Copy and paste this snippet 'deploy ALL=(ALL) NOPASSWD:ALL' in the file about to be opened"
+USER=${1:-deploy}
+
+sudo adduser $USER
+sudo usermod -aG sudo $USER
+echo "Copy and paste this snippet '$USER ALL=(ALL) NOPASSWD:ALL' in the file about to be opened"
 read -n 1 -s -r -p "Press any key to open the file"
 sudo visudo -f /etc/sudoers.d/90-cloud-init-users
 echo "Deploy user setup successfully"
