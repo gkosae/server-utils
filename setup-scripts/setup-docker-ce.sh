dd6509d01b643de4acd193685673e185ff49a76c
George Osae <gkosae@gmail.com>
2021-12-08 15:12:47 +0000

    Update docker setup script

--- setup-scripts/setup-docker-ce.sh
+++ setup-scripts/setup-docker-ce.sh
@@ -12,3 +12,7 @@ sudo apt install docker-ce -y
 sudo systemctl enable docker
 sudo systemctl status docker
 sudo usermod -aG docker ${USER}
+
+# INSTALL DOCKER_COMPOSE
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
