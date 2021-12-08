7ba1ff715b2a14e46f717db83b52f5deeff8c31d
George Osae <gkosae@gmail.com>
2021-12-08 15:57:23 +0000

    Update rails env setup script

--- setup-scripts/setup-rails-env.sh
+++ setup-scripts/setup-rails-env.sh
@@ -21,7 +21,8 @@ echo progress-bar >> ~/.curlrc
 # hkp://pgp.mit.edu
 # hkp://keyserver.pgp.com
 # hkp://pool.sks-keyservers.net (Failing)
-gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+# gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://rvm.io/mpapis.asc | gpg --import - # Keyservers failing so install directly from webservers. Less secure
 \curl -sSL https://get.rvm.io | bash -s stable --with-gems="hirb"
 
 
