faec66ab248e6881af768badf5789ba8814b6f3d
George Osae <gkosae@gmail.com>
2021-12-08 15:03:06 +0000

    Update postgres setup script

--- setup-scripts/setup-postgres10.sh
+++ setup-scripts/setup-postgres10.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 set -e
 
+VERSION=${1:-focal}
+
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ ${VERSION}-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
 sudo apt-get update
 sudo apt-get install -y postgresql-10 postgresql-contrib
