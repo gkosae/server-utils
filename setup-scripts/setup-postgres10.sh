80e1e947f295cc729be05a724e813e41f9a04b64
George Osae <gkosae@gmail.com>
2021-12-08 15:07:53 +0000

    Update docker and postgres setup scripts

--- setup-scripts/setup-postgres10.sh
+++ setup-scripts/setup-postgres10.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 set -e
 
-VERSION=${1:-focal}
-
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt/ ${VERSION}-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
 sudo apt-get update
 sudo apt-get install -y postgresql-10 postgresql-contrib
