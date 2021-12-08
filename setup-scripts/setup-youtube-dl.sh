5f4f352bd1a8f70e1b10f859dde2873ec83a3e16
George Osae <gkosae@gmail.com>
2021-12-08 16:55:56 +0000

    Updated youtube-dl setup script

--- setup-scripts/setup-youtube-dl.sh
+++ setup-scripts/setup-youtube-dl.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -e
 
-sudo apt install ffmpeg ffprobe -y
+sudo apt install ffmpeg -y
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 sudo chmod a+rx /usr/local/bin/yt-dlp
 yt-dlp --version
