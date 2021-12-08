5968b553d572fee4d75eb63598b0dcfe60699dcc
George Osae <gkosae@gmail.com>
2021-12-08 15:54:46 +0000

    Update rails env setup script

--- setup-scripts/setup-rails-env.sh
+++ setup-scripts/setup-rails-env.sh
@@ -17,7 +17,11 @@ sudo apt-get install -y nodejs \
 
 
 echo progress-bar >> ~/.curlrc
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+## KEY SERVERS
+# hkp://pgp.mit.edu
+# hkp://keyserver.pgp.com
+# hkp://pool.sks-keyservers.net (Failing)
+gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable --with-gems="hirb"
 
 
